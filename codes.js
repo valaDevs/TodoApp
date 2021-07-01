99a6259c0ec4dff865cc44b106676657233e0b36
amoVala <vala77fitos@gmail.com>
2021-07-02 00:13:53 +0430

    todo with textnode

---
+++ codes.js
@@ -0,0 +1,23 @@
+
+function add_item() {
+
+    let item = document.getElementById("todo");
+    let list_item = document.querySelector('.list');
+  
+    if(item.value != ""){
+  
+        let make_li = document.createElement("LI");
+        make_li.appendChild(document.createTextNode(item.value));
+        list_item.appendChild(make_li);
+        item.value=""
+            
+        make_li.onclick = function(){
+            this.parentNode.removeChild(this);
+        }
+    }
+    else{
+        alert("plz add a value to item");
+    }
+    
+    }
+    
